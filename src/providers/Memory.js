e71f825dcdecdc4aeed6d4ae087c4750fbf87445
ThatTonybo <twatt74@eq.edu.au>
2019-06-07 11:14:56 +1000

    Add example in-memory provider

---
+++ src/providers/Memory.js
@@ -0,0 +1,44 @@
+const DatabaseProvider = require('../classes/DatabaseProvider');
+
+module.exports = class Memory extends DatabaseProvider {
+    constructor() {
+        super('in-memory');
+
+        this.db = new Map();
+        this.persistent = false;
+    }
+
+    async set(key, value) {
+        this.db.set(key, value);
+        return value;
+    }
+
+    async delete(key) {
+        if (this.db.has(key)) {
+            this.db.delete(key);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async get(key) {
+        if (this.db.has(key)) {
+            return this.db.get(key);
+        } else {
+            return null;
+        }
+    }
+
+    async find(func = null) {
+        if (!func || typeof (func) != 'function') return false;
+        
+        const value = this.db.find(func);
+
+        if (value) {
+            return value;
+        } else {
+            return null;
+        }
+    }
+}
