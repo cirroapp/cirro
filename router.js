06db040f08461679636823b54d0ced421491e933
ThatTonybo <twatt74@eq.edu.au>
2019-06-07 10:39:40 +1000

    Better error handling

--- router.js
+++ router.js
@@ -9,6 +9,11 @@ module.exports = (app) => {
 
     app.use('/', require('./routes/index'));
 
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        return res.status(500).render('errors/500', { stack: err.stack });
+    });
+
     app.use((req, res, next) => {
         return res.status(404).render('errors/404');
     });
