ddc26ac95ac77e7e83f4b7ae43f1f3b4a1a25de3
ThatTonybo <twatt74@eq.edu.au>
2019-06-08 14:38:17 +1000

    Validation improvements, register route and password hashing

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+const randomstring = require('randomstring');
+
 router.route('/login')
 .get(async (req, res) => {
     return res.render('accounts/log-in');
@@ -14,7 +17,20 @@ router.route('/register')
     return res.render('accounts/register');
 })
 .post(async (req, res) => {
-    
+    const { username, password, confirmpassword } = req.body;
+
+    const hashedPassword = bcrypt.hash(password, 10);
+    const id = randomstring.generate({
+        charset: 'numeric'
+    });
+
+    const data = {
+        username,
+        password: hashedPassword
+    }
+
+    await db.set(id, data, 'users');
+    return res.redirect(`/@${username}`);
 });
 
 module.exports = router;
