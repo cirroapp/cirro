e14a2792a329f3f778fbcd9c56c342d47f85bbaf
ThatTonybo <twatt74@eq.edu.au>
2019-06-10 13:51:46 +1000

    Profile improvements, added icons package

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,13 +4,28 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const randomstring = require('randomstring');
 
+const months = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+}
+
 router.get('/@:username', async (req, res) => {
     const username = req.params.username;
 
     const user = await db.find(((d) => d.username == username), 'users');
     if (!user) return;
 
-    return res.render('accounts/profile', { user });
+    return res.render('accounts/profile', { user, months });
 });
 
 router.route('/login')
