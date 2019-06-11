dd2af7df60b3bb3a0bb58d35b05f28d08c2119d8
ThatTonybo <twatt74@eq.edu.au>
2019-06-11 17:14:45 +1000

    Improvements and additions

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,20 @@ const router = express.Router();
 
 const { version } = require('../package.json');
 
-router.get('/', async (req, res) => {
+const checkAdmin = (req, res, next) => {
     if (!req.session.user) return res.status(501).redirect('/');
     if (!req.session.user.admin) return res.status(503).redirect('/');
 
+    next();
+}
+
+router.get('/', checkAdmin, async (req, res) => {
     return res.render('admin/index', { version, startDate });
+});
+
+router.get('/categories', checkAdmin, async (req, res) => {
+    const categories = await global.db
+    return res.render('admin/index', {});
 });
 
 module.exports = router;
